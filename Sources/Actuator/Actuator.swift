66c636af896878e19aec08e01cd3af18ea0c7006
popescun <popescunicolae7@hotmail.com>
2024-04-15 10:44:21 +0200

    update actuator definitions

--- Sources/Actuator/Actuator.swift
+++ Sources/Actuator/Actuator.swift
@@ -1,42 +1,19 @@
 //
 //  Actuator.swift
-//  
+//
 //
 //  Created by Nicolae Popescu on 13/02/2024.
 //
 
-import Foundation
 import ActuatorMacros
 
-public struct Actuator1<R, P1>: ActuatorProtocol {
-  // implement ActuatorProtocol
-  public typealias Action = (P1) -> R
-  // todo: this is a repetitive line that should be in a base class; not sure how to do
-  public var actions: Array<Action> = []
-  
-  // define invoke method for this arity
-  func callAsFunction(_ a: P1) {
-    forEach {
-      let ret = $0(a)
-      print(ret)
-    }
-  }
-}
-
-//public struct Actuator2<R, P1, P2>: ActuatorProtocol {
-//  // implement ActuatorProtocol
-//  public typealias Action = (P1, P2) -> R
-//  public var actions: Array<Action> = []
-//  
-//  // define invoke method for this arity
-//  func callAsFunction(_ a: P1, _ b: P2) {
-//    forEach {
-//      let ret = $0(a, b)
-//      print(ret)
-//    }
-//  }
-//}
-
+#Actuator(1)
+// Alternatively you can use macro with parameters. It expends the same as #Actuator(1).
+//#ActuatorWithParameters(["R", "P1"])
 #Actuator(2)
-
 #Actuator(3)
+#Actuator(4)
+#Actuator(5)
+#Actuator(6)
+#Actuator(7)
+#Actuator(8)
