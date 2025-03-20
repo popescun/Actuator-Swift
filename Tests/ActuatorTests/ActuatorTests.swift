e5aceb4dcf2c1a321056e49fd3ba345f1714db00
popescun <popescunicolae7@hotmail.com>
2025-03-20 16:39:55 +0100

    feat: make Action type identifiable

--- Tests/ActuatorTests/ActuatorTests.swift
+++ Tests/ActuatorTests/ActuatorTests.swift
@@ -6,23 +6,6 @@ import XCTest
 
 @testable import Actuator
 
-func foo(_ a: Int) -> Int {
-    print(a)
-    return a
-}
-
-func foo2(_ a: Int, _ b: Int) -> Int {
-    let ret = a + b
-    print(ret)
-    return ret
-}
-
-func foo3(_ a: Int, _ b: Int, _ c: Int) -> Int {
-    let ret = a + b + c
-    print(ret)
-    return ret
-}
-
 //@freestanding(declaration, names: named(FooMethod))
 //public macro FooMethod(_ : Array<String>) = #externalMacro(module: "ActuatorTests", type: "FooMethodImpl")
 //
@@ -43,28 +26,7 @@ func foo3(_ a: Int, _ b: Int, _ c: Int) -> Int {
 //  }
 //}
 
-// todo: extend tests to cover all possible arities
 final class ActuatorTests: XCTestCase {
-    
-    //    func testExample() throws {
-    //        var actuator1 = Actuator1<Int, Int>([foo])
-    //        actuator1.connect([foo])
-    //        actuator1.actions += [foo]
-    //        actuator1(1)
-    //
-    //        #FooMethod([""])
-    //
-    //        var actuator2 = Actuator2<Int, Int, Int>([foo2])
-    //        actuator2.connect([foo2])
-    //        actuator2.actions += [foo2]
-    //        actuator2(2, 2)
-    //
-    //        var actuator3 = Actuator3<Int, Int, Int, Int>([foo3])
-    //        actuator3.connect([foo3])
-    //        actuator3.actions += [foo3]
-    //        actuator3(3, 3, 3)
-    //    }
-    
     func testPolymorphism() throws {
         //using polymorphism
         print("Using polymorphism:")
@@ -80,34 +42,83 @@ final class ActuatorTests: XCTestCase {
         print("\nUsing Actuator:")
         Expectation.value = expectation(description: "using Actuator")
         Expectation.value.expectedFulfillmentCount = 3
-        var actuator = Actuator1<Void, Int>([a.test])
-        actuator.connect([b.test])
-        actuator.actions += [c.test]
+        var actuator = Actuator1<Void, Int>([Actuator1.Action(action: a.action)])
+        actuator.connect([Actuator1.Action(action: b.action)])
+        actuator.actions += [Actuator1.Action(action: c.action)]
         actuator(11)
         waitForExpectations(timeout: 0)
+        
+        for action in actuator.actions {
+            print("id:", action.id)
+        }
+        
+        // actuator with identifiable actions
+//        var actuator1 = Actuator1<Void, Int>([Actuator1.ActionWrapper(action: a.action)])
+//        print("id:", actuator1.actions1[0].id)
     }
+    
+//    func testCopy() throws {
+//        let a = A()
+//        let b = B()
+//        let c = C()
+//    
+//        Expectation.value = expectation(description: "actuator copy")
+//        Expectation.value.expectedFulfillmentCount = 3
+//        var actuator = Actuator1<Void, Int>()
+//        actuator.connect([a.action, b.action, c.action])
+//        actuator(10)
+//        waitForExpectations(timeout: 0)
+//    }
+//    
+//    func testAdd() throws {
+//        let a = A()
+//        let b = B()
+//        let c = C()
+//    
+//        Expectation.value = expectation(description: "actuator add")
+//        Expectation.value.expectedFulfillmentCount = 4
+//        var actuator = Actuator1<Void, Int>()
+//        actuator.connect([a.action, b.action, c.action])
+//        actuator.actions += [a.action]
+//        actuator(10)
+//        waitForExpectations(timeout: 0)
+//    }
+//    
+//    func testRemove() throws {
+//        let a = A()
+//        let b = B()
+//        let c = C()
+//    
+//        Expectation.value = expectation(description: "actuator add")
+//        Expectation.value.expectedFulfillmentCount = 2
+//        var actuator = Actuator1<Void, Int>()
+//        actuator.connect([a.action, b.action, c.action])
+//        actuator.actions.remove(at: 0)
+//        actuator(10)
+//        waitForExpectations(timeout: 0)
+//    }
 }
 
 protocol P {
-    func test(a: Int)
+    func action(a: Int)
 }
 
 struct A: P {
-    func test(a: Int) {
+    func action(a: Int) {
         print("A.test a: \(a)")
         Expectation.value.fulfill()
     }
 }
 
 struct B: P {
-    func test(a: Int) {
+    func action(a: Int) {
         print("B.test a: \(a)")
         Expectation.value.fulfill()
     }
 }
 
 struct C: P {
-    func test(a: Int) {
+    func action(a: Int) {
         print("C.test a: \(a)")
         Expectation.value.fulfill()
     }
@@ -115,7 +126,7 @@ struct C: P {
 
 func test(list: [P], a: Int) {
     for element in list {
-        element.test(a: a)
+        element.action(a: a)
     }
 }
 
